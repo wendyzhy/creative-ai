6e94b4d4804dba01942588296d52d34683aa2250
njmurray <njmurray@umich.edu>
2016-12-21 15:50:17 -0500

    language-models renamed to language_models

---
+++ language_models/bigramModel.py
@@ -0,0 +1,67 @@
+import random
+from nGramModel import *
+
+
+class BigramModel(NGramModel):
+
+    def __init__(self):
+        """
+        Requires: nothing
+        Modifies: self (this instance of the BigramModel object)
+        Effects:  this is the BigramModel constructor, which is done
+                  for you. It allows BigramModel to access the data
+                  from the NGramModel class by calling the NGramModel
+                  constructor.
+        """
+        super(BigramModel, self).__init__()
+
+    def trainModel(self, text):
+        """
+        Requires: text is a list of lists of strings
+        Modifies: self.nGramCounts, a two-dimensional dictionary. For examples
+                  and pictures of the BigramModel's version of
+                  self.nGramCounts, see the spec.
+        Effects:  this function populates the self.nGramCounts dictionary,
+                  which has strings as keys and dictionaries of
+                  {string: integer} pairs as values.
+
+                  Note: make sure to use the return value of prepData to
+                  populate the dictionary, which will allow the special
+                  symbols to be included as their own tokens in
+                  self.nGramCounts. For more details, see the spec.
+        """
+        pass
+
+    def trainingDataHasNGram(self, sentence):
+        """
+        Requires: sentence is a list of strings, and len(sentence) >= 1
+        Modifies: nothing
+        Effects:  returns True if this n-gram model can be used to choose
+                  the next token for the sentence. For explanations of how this
+                  is determined for the BigramModel, see the spec.
+        """
+        pass
+
+    def getCandidateDictionary(self, sentence):
+        """
+        Requires: sentence is a list of strings, and trainingDataHasNGram
+                  has returned True for this particular language model
+        Modifies: nothing
+        Effects:  returns the dictionary of candidate next words to be added
+                  to the current sentence. For details on which words the
+                  BigramModel sees as candidates, see the spec.
+        """
+        pass
+
+###############################################################################
+# Main
+###############################################################################
+
+if __name__ == '__main__':
+    # Add your test cases here
+    text = [ ['the', 'quick', 'brown', 'fox'], ['the', 'lazy', 'dog'] ]
+    text.append([ 'quick', 'brown' ])
+    sentence = [ 'lazy', 'quick' ]
+    bigramModel = BigramModel()
+
+
---
+++ language_models/trigramModel.py
@@ -0,0 +1,66 @@
+import random
+from nGramModel import *
+
+class TrigramModel(NGramModel):
+
+    def __init__(self):
+        """
+        Requires: nothing
+        Modifies: self (this instance of the NGramModel object)
+        Effects:  this is the TrigramModel constructor, which is done
+                  for you. It allows TrigramModel to access the data
+                  from the NGramModel class.
+        """
+        super(TrigramModel, self).__init__()
+
+    def trainModel(self, text):
+        """
+        Requires: text is a list of lists of strings
+        Modifies: self.nGramCounts, a three-dimensional dictionary. For
+                  examples and pictures of the TrigramModel's version of
+                  self.nGramCounts, see the spec.
+        Effects:  this function populates the self.nGramCounts dictionary,
+                  which has strings as keys and dictionaries as values,
+                  where those inner dictionaries have strings as keys
+                  and dictionaries of {string: integer} pairs as values.
+
+                  Note: make sure to use the return value of prepData to
+                  populate the dictionary, which will allow the special
+                  symbols to be included as their own tokens in
+                  self.nGramCounts. For more details, see the spec.
+        """
+        pass
+
+    def trainingDataHasNGram(self, sentence):
+        """
+        Requires: sentence is a list of strings, and len(sentence) >= 2
+        Modifies: nothing
+        Effects:  returns True if this n-gram model can be used to choose
+                  the next token for the sentence. For explanations of how this
+                  is determined for the TrigramModel, see the spec.
+        """
+        pass
+
+    def getCandidateDictionary(self, sentence):
+        """
+        Requires: sentence is a list of strings, and trainingDataHasNGram
+                  has returned True for this particular language model
+        Modifies: nothing
+        Effects:  returns the dictionary of candidate next words to be added
+                  to the current sentence. For details on which words the
+                  TrigramModel sees as candidates, see the spec.
+        """
+        pass
+
+
+###############################################################################
+# Main
+###############################################################################
+
+if __name__ == '__main__':
+    # Add your tests here
+    text = [ ['the', 'quick', 'brown', 'fox'], ['the', 'lazy', 'dog'] ]
+    sentence = [ 'the', 'quick', 'brown' ]
+    trigramModel = TrigramModel()
+
+
